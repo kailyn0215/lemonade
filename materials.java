c5204bdc4e399b661df8da1b80f8dc9dd85ebe6d
kailyn0215 <brownkai24@7hills.org>
2023-09-18 13:10:34 -0400

    added public vars + ice

--- materials.java
+++ materials.java
@@ -2,13 +2,14 @@ import java.util.Scanner;
 
 public class materials
 {
-    //Public double cupPrice 
+    public double cupPrice = 0.03;
+    public int cups;
     public void shopping(String yes)
     {
-        System.out.print("How many cups would you like to buy?\n- 25 for $0.90\n- 50 for $1.70\n- 100 for $2.76\n\n");
+        System.out.print("How many cups would you like to buy? ($0.03 Each)\n\n");
         Scanner num = new Scanner(System.in);
-        int cups = num.nextInt();
-        System.out.print("How many cups would you like to buy?\n- 25 for $0.90\n- 50 for $1.70\n- 100 for $2.76\n\n");
+        cups = num.nextInt();
+        System.out.print("How much ice would you like to buy?\n- 25 for $0.90\n- 50 for $1.70\n- 100 for $2.76\n\n");
     } 
 }
 
