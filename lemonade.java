0d1fc22b86e1ddc0a1ec941bafe28ea9447a07f8
kailyn0215 <brownkai24@7hills.org>
2023-09-18 12:56:52 -0400

    made it start working!! (so exciting)

--- lemonade.java
+++ lemonade.java
@@ -2,6 +2,7 @@ public class lemonade
 {
     public static void main(String[] args)
     {
-        lemonade 
+        materials day = new materials();
+        day.shopping("Pee pee");
     }   
 }
--- materials.java
+++ materials.java
@@ -1,8 +1,9 @@
 public class materials
 {
-    public void shopping()
+    public void shopping(String yes)
     {
-        
+        System.out.print("How many cups would you like to buy?\n- 25 for $0.90\n- 50 for $1.70\n- 100 for $2.76\n\n");
+        System.out.println(yes);
     } 
 }
 
