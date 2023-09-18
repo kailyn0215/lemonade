7b1b6483a572799ef8239b800298762fdeb7791d
kailyn0215 <brownkai24@7hills.org>
2023-09-18 13:07:37 -0400

    skdfjklfdj

--- materials.java
+++ materials.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
+
 public class materials
 {
+    //Public double cupPrice 
     public void shopping(String yes)
     {
         System.out.print("How many cups would you like to buy?\n- 25 for $0.90\n- 50 for $1.70\n- 100 for $2.76\n\n");
-        System.out.println(yes);
+        Scanner num = new Scanner(System.in);
+        int cups = num.nextInt();
+        System.out.print("How many cups would you like to buy?\n- 25 for $0.90\n- 50 for $1.70\n- 100 for $2.76\n\n");
     } 
 }
 
