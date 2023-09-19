6a3dd9263904cee9468a5af41107cd478c99fa78
kailyn0215 <114490353+kailyn0215@users.noreply.github.com>
2023-09-19 10:58:51 -0400

    added days + method to count them

--- lemonade.java
+++ lemonade.java
@@ -1,8 +1,19 @@
 public class lemonade 
 {
+    public int days;
+    public int count;
     public static void main(String[] args)
     {
+        System.out.println("How many days would you like to play?");
+        Scanner day = new Scanner(System.in);
+        days = day.nextInt();
+        count = 1;
         materials start = new materials();
-        start.shopping(20);
+        while (count == 1)
+            {
+                start.shopping(20);
+            }
+        
+        
     }   
 }
