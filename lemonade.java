ccbde7a64ee7d9c275108f905dc08c11cab287c6
kailyn0215 <114490353+kailyn0215@users.noreply.github.com>
2023-09-19 10:46:43 -0400

    Update lemonade.java

--- lemonade.java
+++ lemonade.java
@@ -2,7 +2,7 @@ public class lemonade
 {
     public static void main(String[] args)
     {
-        materials day = new materials();
-        day.shopping(20);
+        materials start = new materials();
+        start.shopping(20);
     }   
 }
